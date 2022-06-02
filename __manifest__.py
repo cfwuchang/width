b08cb1c5689861caf3c1f731529ad14bc533b973
cfwuchang <67139173+cfwuchang@users.noreply.github.com>
2022-06-02 17:58:32 +0800

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'product_id_whth',
+    'category': 'product_id_whth',
+    'description': """
+            product_id_whth
+            """,
+    'author': 'CodeFire Technologies',
+    'website': 'https://www.codefire.org/',
+    'version': '14.0.1.0',
+    'license': 'AGPL-3',
+    'images': ['static/description/odoo-free.jpg'],
+    'depends': [
+        'base'
+    ],
+    "data": [
+        'views/work_plan_field_width.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
